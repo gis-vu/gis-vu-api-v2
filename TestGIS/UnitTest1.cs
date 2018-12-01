c1e72e613c72c102adbbb3a40c17a14f83b919bd
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-12-01 16:01:12 +0200

    Test project

---
+++ TestGIS/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace TestGIS
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AssertTrue()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AssertFalse()
+        {
+            Assert.True(false);
+        }
+    }
+}
